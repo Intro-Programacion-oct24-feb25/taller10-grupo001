23f632787b2fa49bd51a4305f0903a1ba5ea3a78
andrearj19 <aaarias19@utpl.edu.ec>
2024-12-20 12:31:49 -0500

    cambios

--- Taller10/src/paquete3/Problema03.java
+++ Taller10/src/paquete3/Problema03.java
@@ -14,18 +14,44 @@ public static void main(String[] args) {
     
     String[][] notasCualitativas = new String[3][3];
 	double[][] notasCuantitativas = {{7.1, 8, 10}, {3, 7, 9}, {10, 9, 2.1}};
-     double [] sumaNotas = new double [3];
-     String [] promedioNotasCualitativas = new String [3];
-     double promedios;
-     
-        /*
-        Referencia para obtener el valor cualitativo de una nota numérica
-        0 - 2.9   Insuficiente
-        3 - 4.9   Regular
-        5 - 7.9   Buena
-        8 - 9.5   Muy Buena
-        9.6 - 10  Sobresaliente  
-        */
-        
-}
+    
+        for (int i = 0; i < notasCuantitativas.length; i++) {
+            for (int j = 0; j < notasCuantitativas[i].length; j++) {
+                double nota = notasCuantitativas[i][j]; 
+
+                
+                if (nota >= 0 && nota <= 2.9) {
+                    notasCualitativas[i][j] = "Insuficiente";
+                } else if (nota >= 3 && nota <= 4.9) {
+                    notasCualitativas[i][j] = "Regular";
+                } else if (nota >= 5 && nota <= 7.9) {
+                    notasCualitativas[i][j] = "Buena";
+                } else if (nota >= 8 && nota <= 9.5) {
+                    notasCualitativas[i][j] = "Muy Buena";
+                } else if (nota > 9.5 && nota <= 10) {
+                    notasCualitativas[i][j] = "Sobresaliente";
+                } else {
+                    notasCualitativas[i][j] = "Nota Inválida"; 
+                }
+            }
+        }
+
+  
+        System.out.println("Notas Cuantitativas:");
+        for (double[] fila : notasCuantitativas) {
+            for (double nota : fila) {
+                System.out.print(nota + "\t");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\nNotas Cualitativas:");
+        for (String[] fila : notasCualitativas) {
+            for (String nota : fila) {
+                System.out.print(nota + "\t");
+            }
+            System.out.println();
+        }
+    }
+      
 }
--- Taller10/src/paquete4/Problema04.java
+++ Taller10/src/paquete4/Problema04.java
@@ -5,10 +5,44 @@
  */
 package paquete4;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Problema04 {
-    
+     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double[][] datos = new double[2][2];
+        int fueraDeRango = 0; 
+
+        
+        for (int i = 0; i < datos.length; i++) {
+            for (int j = 0; j < datos[i].length; j++) {
+                System.out.print("Ingrese un número para datos[" + i + "][" + j + "]: ");
+                double valor = scanner.nextDouble();
+
+                if (valor >= 1000 && valor <= 1199) {
+                    datos[i][j] = valor; 
+                } else {
+                    datos[i][j] = 10; 
+                    fueraDeRango++;  
+                }
+            }
+        }
+
+      
+        System.out.println("\nContenido del arreglo:");
+        for (int i = 0; i < datos.length; i++) {
+            for (int j = 0; j < datos[i].length; j++) {
+                System.out.print(datos[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+       
+        System.out.println("\nCantidad de valores fuera de rango: " + fueraDeRango);
+    }
 }
+
