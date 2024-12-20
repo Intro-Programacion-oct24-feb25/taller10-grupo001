8d1aaeccb8920cd0962b547244eb630086b8f9d3
andrearj19 <aaarias19@utpl.edu.ec>
2024-12-20 10:56:00 -0500

    problema04

--- Taller10/src/paquete4/Problema04.java
+++ Taller10/src/paquete4/Problema04.java
@@ -4,11 +4,43 @@
  * and open the template in the editor.
  */
 package paquete4;
-
 /**
  *
  * @author reroes
  */
+import java.util.Scanner;
 public class Problema04 {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double[][] datos = new double[2][2];
+        int fueraRango = 0; 
+
+        
+        for (int i = 0; i < datos.length; i++) {
+            for (int j = 0; j < datos[i].length; j++) {
+                System.out.print("Ingrese un número para datos[" + i + "][" + j + "]: ");
+                double valor = scanner.nextDouble();
+
+                if (valor >= 1000 && valor <= 1199) {
+                    datos[i][j] = valor; 
+                } else {
+                    datos[i][j] = 10; 
+                    fueraRango++;   
+                }
+            }
+        }
+
+      
+        System.out.println("\nContenido del arreglo:");
+        for (double[] dato : datos) {
+            for (int j = 0; j < dato.length; j++) {
+                System.out.print(dato[j] + "\t");
+            }
+            System.out.println();
+        }
+
+       
+        System.out.println("\nCantidad de valores fuera de rango: " + fueraRango);
+    }
 }
+
